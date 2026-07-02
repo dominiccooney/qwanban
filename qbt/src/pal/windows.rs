24bb6f10e3170972a758883bb4a30d91589368c6
Dominic Cooney <dominic.cooney@gmail.com>
2026-07-02 15:44:04 +0900

    Use impl Drop to manage resources.

--- qbt/src/pal/windows.rs
+++ qbt/src/pal/windows.rs
@@ -1,10 +1,6 @@
-use anyhow::{anyhow, bail};
-use windows::Win32::Foundation::{ERROR_INVALID_PARAMETER, RECT};
-use windows::Win32::Graphics::Gdi::{
-    BI_RGB, BITMAPINFO, BITMAPINFOHEADER, BitBlt, CreateCompatibleBitmap, CreateCompatibleDC,
-    DIB_RGB_COLORS, DeleteDC, DeleteObject, GetDC, GetDIBits, HDC, ReleaseDC, SRCCOPY,
-    SelectObject,
-};
+use anyhow::{anyhow, bail, Context};
+use windows::Win32::Foundation::{HWND, ERROR_INVALID_PARAMETER, RECT};
+use windows::Win32::Graphics::Gdi::{BI_RGB, BITMAPINFO, BITMAPINFOHEADER, BitBlt, CreateCompatibleBitmap, CreateCompatibleDC, DIB_RGB_COLORS, DeleteDC, DeleteObject, GetDC, GetDIBits, HDC, ReleaseDC, SRCCOPY, SelectObject, HGDIOBJ, HBITMAP};
 use windows::Win32::UI::HiDpi::{PROCESS_PER_MONITOR_DPI_AWARE, SetProcessDpiAwareness};
 use windows::Win32::UI::WindowsAndMessaging::{
     CURSOR_SHOWING, CURSORINFO, DI_NORMAL, DrawIconEx, GetCursorInfo, GetDesktopWindow,
@@ -13,51 +9,140 @@ use windows::Win32::UI::WindowsAndMessaging::{
 
 type ScreenshotImage = image::ImageBuffer<image::Rgba<u8>, Vec<u8>>;
 
+struct DesktopDC {
+    hwnd: HWND,
+    hdc: HDC,
+}
+
+impl DesktopDC {
+    unsafe fn get() -> anyhow::Result<DesktopDC> {
+        unsafe {
+            let hwnd = GetDesktopWindow();
+            if hwnd.is_invalid() {
+                bail!("failed to get desktop window");
+            }
+            let hdc = GetDC(Some(hwnd));
+            if hdc.is_invalid() {
+                ReleaseDC(None, hdc);
+                bail!("failed to get screen DC");
+            }
+            Ok(Self {
+                hwnd,
+                hdc,
+            })
+        }
+    }
+}
+
+impl Drop for DesktopDC {
+    fn drop(&mut self) {
+        unsafe {
+            ReleaseDC(Some(self.hwnd), self.hdc);
+        }
+    }
+}
+
+struct OwnedHBITMAP {
+    hbitmap: HBITMAP,
+}
+
+impl OwnedHBITMAP {
+    fn adopt(hbitmap: HBITMAP) -> anyhow::Result<Self> {
+        if hbitmap.is_invalid() {
+            bail!("bitmap handle is invalid");
+        }
+        Ok(Self { hbitmap })
+    }
+}
+
+impl Drop for OwnedHBITMAP {
+    fn drop(&mut self) {
+        unsafe {
+            #[allow(unused_must_use)]
+            DeleteObject(self.hbitmap.into());
+        }
+    }
+}
+
+struct OwnedHDC {
+    hdc: HDC
+}
+
+impl OwnedHDC {
+    fn adopt(hdc: HDC) -> anyhow::Result<Self> {
+        if hdc.is_invalid() {
+            bail!("HDC is invalid");
+        }
+        Ok(Self {
+            hdc
+        })
+    }
+}
+
+impl Drop for OwnedHDC {
+    fn drop(&mut self) {
+        unsafe {
+            #[allow(unused_must_use)]
+            DeleteDC(self.hdc);
+        }
+    }
+}
+
+struct DeviceContextBitmapSwitcheroo<'a> {
+    hdc: &'a HDC,
+    old_bitmap: HGDIOBJ,
+}
+
+impl <'a> DeviceContextBitmapSwitcheroo<'a> {
+    // Note: The extensive lifetime on new_bitmap is because we will use the bitmap until we
+    // swap the old one back, even though we're not holding onto the *handle* anywhere.
+    unsafe fn select(hdc: &'a HDC, new_bitmap: &'a HBITMAP) -> Self {
+        unsafe {
+            let old_bitmap = SelectObject(*hdc, (*new_bitmap).into());
+            Self {
+                hdc,
+                old_bitmap,
+            }
+        }
+    }
+}
+
+impl <'a> Drop for DeviceContextBitmapSwitcheroo<'a> {
+    fn drop(&mut self) {
+        unsafe {
+            SelectObject(*self.hdc, self.old_bitmap);
+        }
+    }
+}
+
 pub(crate) fn screenshot() -> anyhow::Result<ScreenshotImage> {
     unsafe {
         #[allow(unused_must_use)]
         SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
 
         // Get the desktop and dimensions
-        let hwnd_desktop = GetDesktopWindow();
-        if hwnd_desktop.is_invalid() {
-            bail!("failed to get desktop window");
-        }
+        let desktop = DesktopDC::get()?;
         let mut rect = RECT::default();
-        GetWindowRect(hwnd_desktop, &mut rect)?;
+        GetWindowRect(desktop.hwnd, &mut rect)?;
         let (width, height) = (rect.right, rect.bottom);
 
-        // Set up the GDI device context
-        let hdc_screen = GetDC(Some(hwnd_desktop));
-        if hdc_screen.is_invalid() {
-            ReleaseDC(None, hdc_screen);
-            bail!("failed to get screen DC");
-        }
-
-        let h_bitmap = CreateCompatibleBitmap(hdc_screen, width, height);
-        if h_bitmap.is_invalid() {
-            bail!("failed to create bitmap");
-        }
-
-        let hdc_memory = CreateCompatibleDC(Some(hdc_screen));
-        if hdc_memory.is_invalid() {
-            bail!("failed to create memory DC");
-        }
-        let h_old_bitmap = SelectObject(hdc_memory, h_bitmap.into());
+        let h_bitmap = OwnedHBITMAP::adopt(CreateCompatibleBitmap(desktop.hdc, width, height)).context("creating bitmap to copy screen contents")?;
+        let hdc_memory = OwnedHDC::adopt(CreateCompatibleDC(Some(desktop.hdc))).context("creating memory device context")?;
+        let switch = DeviceContextBitmapSwitcheroo::select(&hdc_memory.hdc, &h_bitmap.hbitmap);
 
         // Copy and extract pixels
         BitBlt(
-            hdc_memory,
+            hdc_memory.hdc,
             0,
             0,
             width,
             height,
-            Some(hdc_screen),
+            Some(desktop.hdc),
             rect.left,
             rect.top,
             SRCCOPY,
         )?;
-        draw_cursor_to_dc(hdc_memory, rect.left, rect.top)?;
+        draw_cursor_to_dc(hdc_memory.hdc, rect.left, rect.top)?;
 
         let mut bmi = BITMAPINFO {
             bmiHeader: BITMAPINFOHEADER {
@@ -74,8 +159,8 @@ pub(crate) fn screenshot() -> anyhow::Result<ScreenshotImage> {
 
         let mut pixels = vec![0u8; (width * height * 4) as usize];
         let get_dib_bits_result = GetDIBits(
-            hdc_memory,
-            h_bitmap,
+            hdc_memory.hdc,
+            h_bitmap.hbitmap,
             0,
             height as u32,
             Some(pixels.as_mut_ptr() as *mut _),
@@ -87,14 +172,10 @@ pub(crate) fn screenshot() -> anyhow::Result<ScreenshotImage> {
         }
 
         // Clean up
-        SelectObject(hdc_memory, h_old_bitmap);
-        if !DeleteObject(h_bitmap.into()).as_bool() {
-            anyhow::bail!("failed to delete bitmap");
-        }
-        if !DeleteDC(hdc_memory).as_bool() {
-            anyhow::bail!("failed to delete DC");
-        }
-        ReleaseDC(hwnd_desktop.into(), hdc_screen);
+        drop(switch);
+        drop(h_bitmap);
+        drop(hdc_memory);
+        drop(desktop);
 
         // Convert from BGRA to RGBA PNG
         for chunk in pixels.chunks_exact_mut(4) {
