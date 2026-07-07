0b6a51fdbb659b95c88555412ea798654f024996
Dominic Cooney <dominic.cooney@gmail.com>
2026-07-07 11:31:32 +0900

    Add mouse movement to the input demo.

--- qbt/src/input.rs
+++ qbt/src/input.rs
@@ -9,5 +9,15 @@ pub(crate) async fn send_input_demo() -> anyhow::Result<()> {
         pal::send_key_up(key)?;
         tokio::time::sleep(Duration::from_millis(30)).await;
     }
+
+    let (start_x, start_y, end_x, end_y) = (10.0, 400.0, 300.0, 200.0);
+
+    for i in 0..100 {
+        let t = i as f32 / 100.0;
+        let x = start_x + t * (end_x - start_x);
+        let y = start_y + t * (end_y - start_y);
+        pal::mouse_move_to((x as i32, y as i32)).await?;
+    }
+
     Ok(())
 }
--- qbt/src/pal/windows/input.rs
+++ qbt/src/pal/windows/input.rs
@@ -1,4 +1,5 @@
-use winput::{Vk, Input};
+use std::time::Duration;
+use winput::{Mouse, Input};
 
 pub(crate) type Key = char;
 
@@ -23,6 +24,47 @@ pub(crate) fn send_key_up(ch: Key) -> anyhow::Result<()> {
         None => anyhow::bail!("could not find key for character '{}'", ch),
         Some(input) => input,
     };
+    if winput::send_inputs(&[input]) == 1 {
+        Ok(())
+    } else {
+        Err(winput::WindowsError::from_last_error().into())
+    }
+}
+
+pub(crate) async fn mouse_move_to((x, y): (i32, i32)) -> anyhow::Result<()> {
+    // TODO: Add acceleration and jitter so bot detection isn't triggered by this.
+    // TODO: Synthesize actual inputs and return errors.
+    Mouse::set_position(x, y)?;
+    tokio::time::sleep(Duration::from_millis(8)).await;
+    Ok(())
+}
+
+pub(crate) enum MouseButton {
+    Left,
+    Right,
+}
+
+impl MouseButton {
+    fn to_winput(self) -> winput::Button {
+        match self {
+            MouseButton::Left => winput::Button::Left,
+            MouseButton::Right => winput::Button::Right,
+        }
+    }
+}
+
+// TODO: Modifier clicks.
+pub(crate) async fn mouse_down(button: MouseButton) -> anyhow::Result<()> {
+    let input = Input::from_button(button.to_winput(), winput::Action::Press);
+    if winput::send_inputs(&[input]) == 1 {
+        Ok(())
+    } else {
+        Err(winput::WindowsError::from_last_error().into())
+    }
+}
+
+pub(crate) async fn mouse_up(button: MouseButton) -> anyhow::Result<()> {
+    let input = Input::from_button(button.to_winput(), winput::Action::Release);
     if winput::send_inputs(&[input]) == 1 {
         Ok(())
     } else {
